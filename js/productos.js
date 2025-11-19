0d0799579b02d54d719fc04f884c35213710614c
JoseMPaz <jpazportilla@frba.utn.edu.ar>
2025-11-19 19:37:40 -0300

    cambio imagenes

--- js/productos.js
+++ js/productos.js
@@ -1,8 +1,8 @@
 //⚠️Detalle: rutas ABSOLUTAS ya que ahora se usaran desde 2 html en distintas ubicaciones
 export const productos = [
-  { id: "1", nombre: "The Finals game", img: "img/thefinals.png", precio: 0 },
-  { id: "2", nombre: "DLC: Drifter", img: "img/Drifter.png", precio: 20 },
-  { id: "3", nombre: "DLC: IVADA", img: "img/ivada.png", precio: 20 },
-  { id: "4", nombre: "DLC: Ops mset", img: "img/Ops.png", precio: 20 },
-  { id: "5", nombre: "Arc Raiders", img: "img/arc.png", precio: 40 },
+  { id: "1", nombre: "Cuadro", img: "img/cuadro.jpg", precio: 250000 },
+  { id: "2", nombre: "Cadena", img: "img/cadena.jpeg", precio: 10000 },
+  { id: "3", nombre: "Cubierta", img: "img/cubierta.jpeg", precio: 20 },
+  { id: "4", nombre: "Horquilla", img: "img/horquilla.jpeg", precio: 20 },
+  { id: "5", nombre: "Plato y Palanca", img: "img/plato-palanca.jpeg", precio: 40 },
 ];
