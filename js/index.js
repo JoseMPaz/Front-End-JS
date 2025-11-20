329482426570d690fbd631622b0e4e2df9925e20
JoseMPaz <jpazportilla@frba.utn.edu.ar>
2025-11-19 22:57:02 -0300

    agrego fetch

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-import { productos } from "./productos.js";
 import { agregarAlCarrito } from "./funcionesCarrito.js";
 import { obtenerCarrito } from "./storage.js";
 import { actualizarContador } from "./ui.js";
@@ -6,7 +5,8 @@ import { actualizarContador } from "./ui.js";
 //El evento "DOMContentLoaded" sirve para que no intentemos acceder a un nodo HTML con el
 //  codigo js antes de que el navegador lo cree:
 //Por ejemplo: que no lea un getElementById cuando aun no existe ese id.
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => 
+{
   //Accedemos al contenedor donde queremos generar los articles
   const contenedor = document.getElementById("contenedor-tarjetas");
 
@@ -14,35 +14,50 @@ document.addEventListener("DOMContentLoaded", () => {
   const carrito = obtenerCarrito();
   actualizarContador(carrito);
 
-  productos.forEach((producto) => {
-    // creamos los articles y sus contenidos
-    const tarjeta = document.createElement("article");
-    tarjeta.classList.add("tarjeta-producto");
+  fetch("./data/productos.json")
+    .then ( response => 
+    {
+        if (!response.ok)
+        {
+          throw new Error(`Error al obtener los productos, número de estado: ${response.status}`);
+        }
+        return response.json();
+        
+    } )
+    .then(productos => 
+    {
+      productos.forEach((producto) => 
+      {
+        // creamos los articles y sus contenidos
+        const tarjeta = document.createElement("article");
+        tarjeta.classList.add("tarjeta-producto");
 
-    const img = document.createElement("img");
-    img.src = `./${producto.img}`;
-    img.alt = producto.nombre;
+        const img = document.createElement("img");
+        img.src = `./${producto.img}`;
+        img.alt = producto.nombre;
 
-    const titulo = document.createElement("h3");
-    titulo.textContent = producto.nombre;
+        const titulo = document.createElement("h3");
+        titulo.textContent = producto.nombre;
+        const precio = document.createElement("p");
+        precio.textContent = `$${producto.precio}`;
 
-    const precio = document.createElement("p");
-    precio.textContent = `$${producto.precio}`;
+        const boton = document.createElement("button");
+        boton.classList.add("btn");
+        boton.textContent = "Agregar al carrito";
 
-    const boton = document.createElement("button");
-    boton.classList.add("btn");
-    boton.textContent = "Agregar al carrito";
+        boton.addEventListener("click", () => 
+        {
+          agregarAlCarrito(producto);
+        });
 
-    boton.addEventListener("click", () => {
-      agregarAlCarrito(producto);
-    });
-
-    // Armar la estructura
-    tarjeta.appendChild(img);
-    tarjeta.appendChild(titulo);
-    tarjeta.appendChild(precio);
-    tarjeta.appendChild(boton);
-
-    contenedor.appendChild(tarjeta);
-  });
-});
+        // Armar la estructura
+        tarjeta.appendChild(img);
+        tarjeta.appendChild(titulo);
+        tarjeta.appendChild(precio);
+        tarjeta.appendChild(boton);
+        contenedor.appendChild(tarjeta);
+      }
+    );
+  } )
+    .catch(error => console.error("Error al cargar los productos:", error));
+} ); 
--- js/productos.js
+++
@@ -1,8 +0,0 @@
-//⚠️Detalle: rutas ABSOLUTAS ya que ahora se usaran desde 2 html en distintas ubicaciones
-export const productos = [
-  { id: "1", nombre: "Cuadro", img: "img/cuadro.jpg", precio: 250000 },
-  { id: "2", nombre: "Cadena", img: "img/cadena.jpeg", precio: 10000 },
-  { id: "3", nombre: "Cubierta", img: "img/cubierta.jpeg", precio: 20 },
-  { id: "4", nombre: "Horquilla", img: "img/horquilla.jpeg", precio: 20 },
-  { id: "5", nombre: "Plato y Palanca", img: "img/plato-palanca.jpeg", precio: 40 },
-];
