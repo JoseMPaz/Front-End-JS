026685c9c9f9976c26740cf574c77dd7a05827b8
JoseMPaz <jpazportilla@frba.utn.edu.ar>
2025-11-20 01:27:59 -0300

    modifico carrito

--- js/storage.js
+++ js/storage.js
@@ -1,15 +1,19 @@
 const KEY = "carrito";
 
-export const guardarCarrito = (carrito) => {
+export const guardarCarrito = (carrito) => 
+{
   //Convertimos a json antes de guardar con stringify
   localStorage.setItem(KEY, JSON.stringify(carrito));
 };
 
-export const obtenerCarrito = () => {
+export const obtenerCarrito = () => 
+{
+	const carritoJSON = localStorage.getItem(KEY);
   //convertimos a js para obtener los datos con parse
-  return JSON.parse(localStorage.getItem(KEY)) || [];
+  return carritoJSON ? JSON.parse(carritoJSON) : [];
 };
 
-export const vaciarCarritoStorage = () => {
+export const vaciarCarritoStorage = () => 
+{
   localStorage.removeItem(KEY);
 };
